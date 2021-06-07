1d384608dba77478b9a5002a72f483a9193dc687
Teri Klosky <t.klosky@yahoo.com>
2021-06-07 10:47:12 -0400

    finished

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ let colaButton = document.querySelector(".cola");
 let peanutButton = document.querySelector(".peanuts");
 let chocolateButton = document.querySelector(".chocolate");
 let gummiesButton = document.querySelector(".gummies");
+let moneyForm = document.querySelector(".money-form");
+let coinContainer = document.querySelector(".coin-container");
+let bulb = document.querySelector(".bulb");
+let on = document.querySelector(".on");
+let off = document.querySelector(".off");
+let toggle = document.querySelector(".toggle");
+let end = document.querySelector(".end");
+let buttons = document.querySelectorAll(".button");
 
 colaButton.addEventListener("click", () => {
   let amount = parseFloat(colaButton.getAttribute("data-amount"));
@@ -15,8 +23,8 @@ colaButton.addEventListener("click", () => {
 });
 
 peanutButton.addEventListener("click", () => {
-  let amount = parseFloat(peanutButton.getAttribute("data-amount"));
-  total += amount;
+  let amount = parseFloat(peanutButton.getAttribute("data-amount")); //get the amount from the attribute
+  total += amount; //use that number to increment
   console.log(total);
   totalParagraph.innerText = `Total:$${total}`;
 });
@@ -35,22 +43,38 @@ gummiesButton.addEventListener("click", () => {
   totalParagraph.innerText = `Total:$${total}`;
 });
 
-{
-  let formContainer = document.querySelector(".form-container");
-  let coinCount = document.querySelector(".how-many");
-  let coinName = document.querySelector(".which-coin");
-  let coinContainer = document.querySelector(".coins");
+moneyForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-  formContainer.addEventListener("submit", (event) => {
-    event.preventDefault();
+  let amount = document.querySelector("#amount").value;
+  let type = document.querySelector("#type").value; //gets the value from input fields
+  console.log(amount, type);
 
-    formContainer.addEventListener("click", (event) => {
-      for (let i = 0; i < coinCount; i++) {
-        let newCoin = document.createElement("div");
-        newCoin.innerText = coinName;
-        newCoin.classList.add("coin");
-        coinContainer.append(newCoin);
-      }
+  for (let i = 0; i < amount; i++) {
+    let newCoin = document.createElement("div");
+    newCoin.classList.add("coin", type);
+    newCoin.innerText = type;
+    coinContainer.append(newCoin);
+    newCoin.addEventListener("click", () => {
+      newCoin.remove();
     });
+  }
+});
+
+on.addEventListener("click", () => {
+  bulb.classList.add("light");
+});
+
+off.addEventListener("click", () => {
+  bulb.classList.remove("light");
+});
+
+toggle.addEventListener("click", () => {
+  bulb.classList.toggle("light");
+});
+
+end.addEventListener("click", () => {
+  buttons.forEach((button) => {
+    button.disabled = true;
   });
-}
+});
